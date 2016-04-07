a47233d6e54077e7e1e45508c7fddca207003bb8
Ben Clark <ben.clark@jacksonriver.com>
2016-04-07 16:17:31 -0400

    Remove default endpoint attribute.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,1 @@
-default["sumologic"]["endpoint"] = "https://collectors.sumologic.com/receiver/v1/http/ZaVnC4dhaV01NFJRMJ0OShC_OgI8kaQ-gmkpn1T3lIh2O3OBRlBaukYRfo_U0qiW8oBCgZk7XTkGJXzjmgOMG0JGKGtsoZEN2hM_MCb3BJkPO8r1-ippWA=="
-
+default["sumologic"]["endpoint"] = nil
